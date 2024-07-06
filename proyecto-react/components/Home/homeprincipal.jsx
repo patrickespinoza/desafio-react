12bd8f80c87658050bfd9980a20c7552d13a1421
patrick espinoza <patrickespinoza38@gmail.com>
2024-07-05 23:15:15 -0600

    ultimos cambio solo son detalles en la pagina princila y readme

--- proyecto-react/components/Home/homeprincipal.jsx
+++ proyecto-react/components/Home/homeprincipal.jsx
@@ -35,41 +35,47 @@ export default function HomePrincipal() {
     if (error) {
       return <p>Error: {error}</p>;
     }
+
+    const formatDate = (dateString) => {
+      const options = { month: 'long', day: 'numeric' };
+      return new Date(dateString).toLocaleDateString('es-ES', options);
+    };
   
     return (
-      <div className="container">
-        <div className="flex text-start text-2xl">
-          <h1 className="text-center mb-5 font-bold mr-4">Relevant</h1>
-          <h2 className="flex mr-4">Latest</h2>
-          <h2 className="flex- mr-4">Top</h2>
+      <div className="container mx-auto p-4 ">
+        <div className="flex text-start text-2xl mb-5">
+          <h1 className="font-bold mr-4">Relevant</h1>
+          <h2 className="mr-4">Latest</h2>
+          <h2 >Top</h2>
         </div>
-        <div className="row">
+        <div className="grid grid-cols-1 gap-4">
           {posts.length === 0 ? (
             <p>No posts available</p>
           ) : (
-            posts.map((post) => (
-              <div key={post._id} className="col-md-12 mb-4 border">
-                <div className="card">
-                  <div className="card-body">
-                    {post.image && <img src={post.image} alt="Post Image" className="mb-4" />}
-                    <p>{new Date(post.createdate).toLocaleDateString()}</p>
-                    <h2 className="card-title">
+            posts.map((post, index) => (
+              <div key={post._id} className="border p-4 rounded-lg flex flex-col justify-between min-h[400px]">
+                <div>
+
+                    {index === 0 && post.image && (<img src={post.image} alt="Post Image" className="mb-4 h-70 w-full object-cover rounded-lg" />)}
+                    <img src={`https://api.dicebear.com/9.x/fun-emoji/svg?seed=${post._id}`} alt="post id imagen" className='mb-4 h-8 w-8 object-cover rounded-full'/>
+
+                    <p className='mb-2'>{formatDate(post.createdate)}</p>
+
+                    <h2 className="font-bold text-xl mb-2">
                       <Link href={`/${post._id}`}>
-                    <h3 className="card-subtitle mb-2 text-muted font-bold text-2xl hover:text-blue-700">{post.title}</h3>
+                    <h3 className=" hover:text-blue-700">{post.title}</h3>
                         
                       </Link>
                     </h2>
-                    <div className='flex flex-row p-4'>
-                    <p>&#128588; &#128150; &#129327; &#128293; </p>
-                    
-                    <p>10 Reactions</p>
-                    <p className='px-5'> 	&#128172; 4 comment </p>
-                    <p className='px-5'> 3 min read</p>
+                  </div>
+                    <div className='flex flex-row items-center mt-4'>
+                    <p className='mr-2'>&#128588; &#128150; &#129327; &#128293; </p>
+                    <p className='mr-2'>10 Reactions</p>
+                    <p className='mr-2'> 	&#128172; 4 comment </p>
+                    <p> 3 min read</p>
 
-                    </div>
                   </div>
                 </div>
-              </div>
             ))
           )}
         </div>
--- proyecto-react/pages/[id].jsx
+++ proyecto-react/pages/[id].jsx
@@ -26,17 +26,27 @@ export default function PostDetail() {
   if (!post) {
     return <div>Cargando...</div>;
   }
+  
+  const formatDate = (dateString) => {
+    const options = { month: 'long', day: 'numeric' };
+    return new Date(dateString).toLocaleDateString('es-ES', options);
+  };
 
   return (
-    <main className="flex justify-center items-center min-h-screen">
-      <article className="p-6 max-w-md w-full text-center">
+    <main className="flex justify-center items-center min-h-screen gap-6">
+      <article className="p-6 max-w-7xl w-full text-center">
+        <div className="relative"> 
+
         {post.image && (
           <img className="w-full h-auto rounded-lg mb-4" src={post.image} alt="Post Image" />
         )}
-        <p className="text-gray-700 mb-2 text-start p-2">Created Date: {new Date(post.createdate).toLocaleDateString()}</p>
-        <p className="flex justify-between">&#128588; &#128150; &#129327; &#128293; </p>
-        <h1 className="text-2xl font-bold mb-2">{post.title}</h1>
-        <p className="text-gray-700 mb-2 p-6">{post.body}</p>
+         <img src={`https://api.dicebear.com/9.x/fun-emoji/svg?seed=${post._id}`} alt="post id imagen" className='ml-4 h-8 w-8 object-cover rounded-full'/>
+        </div>
+
+         <p className='text-left mb-2'>{formatDate(post.createdate)}</p>
+        <p className="flex justify-between mb-2">&#128588; &#128150; &#129327; &#128293; </p>
+        <h1 className="font-bold mb-2 text-3xl">{post.title}</h1>
+        <p className="text-gray-700 mb-2 p-6 text-2xl">{post.body}</p>
       </article>
     </main>
   );
